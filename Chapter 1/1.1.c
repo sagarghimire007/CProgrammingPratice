79f8808b1d58005c6f1bd6de603c33bcf55715ea
Sagar <sagarghimire137@gmail.com>
2021-09-27 23:02:53 +0545

    Committed

---
+++ Chapter 1/1.1.c
@@ -0,0 +1,11 @@
+// displaying the equation of line in form ax+by = c
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<string.h>
+int main()
+{
+    int a = 5,b= 8,c=18;
+    printf("The equation of line in form ax+by = c :\n %dx +%dy = %d",a,b,c);
+    return 0;
+}
---
+++ Chapter 3/arrayMaxCalculate.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+int main()
+{
+    int a = 15;
+    if(a == 15)
+        printf("%d",a);
+    else if(a == )
+    return 0;
+}
